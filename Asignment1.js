56af10ace840f0cfc97513afa6b0a32bff29d7dc
rishav87 <105482588+rishav87@users.noreply.github.com>
2022-12-31 22:28:05 +0530

    Add files via upload

---
+++ Asignment1.js
@@ -0,0 +1,66 @@
+//Solution of Question 1
+//comments can make code readable
+
+//Solution of Question 2
+
+//Welcome to 30DaysOfJavaScript
+
+//Solution of Question 3
+
+/*comments can make code readable
+easy to
+reuse and informative*/
+
+
+//Solution of question 6
+var fname;
+var roll;
+var lname;
+var grade;
+
+//Solution of question 7
+
+var fname = Rishav;
+var lname = Sinha;
+var roll = 123;
+var knowsPython = false;
+
+//Solution of Question 8
+var firstName = "Rishav";
+var lastName = "Sinha";
+var age =  20;
+var country = "India";
+var maritalStatus = "Unmarried";
+
+//Solution of Question 9
+
+let [firstName, lastName, age,country,maritalStatus] = ["Rishav", "Sinha", 20 , "India", "Unmarried"];
+
+//Solution of Question 10
+var myAge = 25;
+var yourAge = 30;
+
+console.log(`
+    I am : ${myAge} years old.
+    You are : ${yourAge} years old.
+
+`)
+
+//Solution of Question 4
+var name = "Rishav";
+var age = 20
+var knowsPython = false
+var address;
+var frnds = null;
+
+//Solution of Question 5
+var name = "Rishav";
+var age = 12;
+var knowsPython = false;
+var frnds;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof knowsPython);
+console.log(typeof frnds);
+
